6461446c36225e99ff68622a8199e13f741425ef
哈悠 <hayou@juanpi.com>
2018-07-25 15:38:51 +0800

    配置

--- .config.js
+++ .config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   // devServer: 'http://localhost:3000',
-  devServer: 'http://www.niurouzhou.com/',
-  proServer: 'www.niurouzhou.com',
+  devServer: 'http://www.yupianzhou.com/',
+  proServer: 'www.yupianzhou.com',
 };
