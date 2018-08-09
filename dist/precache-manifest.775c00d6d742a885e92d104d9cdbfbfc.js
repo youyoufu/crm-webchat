8086ccf6365c2aab69391f809bde8de9b18b0b30
hill <hill@stanleydeMacBook-Pro.local>
2018-08-09 09:48:46 +0800

    回滚

---
+++ dist/precache-manifest.775c00d6d742a885e92d104d9cdbfbfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "76e03dd3f11cc751f212",
+    "url": "/js/vendor.76e03dd3.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "c1b72d27f7dfddb4bf74",
+    "url": "/js/app.c1b72d27.js"
+  },
+  {
+    "revision": "3da70c2e673e398be8db3140fabf40fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "162f9a0939e9dda8b23b70e4241c6646",
+    "url": "/img/demo.162f9a09.png"
+  },
+  {
+    "revision": "c1b72d27f7dfddb4bf74",
+    "url": "/css/app.257664cd.css"
+  }
+];
